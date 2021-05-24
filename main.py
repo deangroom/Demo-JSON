57659d30e232cd18a3ddf72ccc9f3c7a1e4a1000
outttatime <>
2021-05-24 04:45:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import json
+with open('Stats.json') as stats:
+ data = json.load(stats)
+ print(data)
+Strength = input("How much strength? ")
+data["Strength: "] = Strength
+Constitution = input("How much constituion? ")
+data["Constitution: "] = Constitution
+Dexterity = input("How much Dexterity? ")
+data["Dexterity: "] = Dexterity
+Intelligence = input("How much Intelligence? ")
+data["Intelligence: "] = Intelligence
+Wisdom = input("How much Wisdom? ")
+data["Wisdom: "] = Wisdom
+Charisma = input("How much Charisma? ")
+data["Charisma: "] = Charisma
+print(data)
